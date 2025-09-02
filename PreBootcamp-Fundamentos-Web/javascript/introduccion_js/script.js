cc2c18709f709e7b273d12e0ad1c568135301e58
Sr Jengibre <spobleteobando@gmail.com>
2025-09-01 21:04:50 -0400

    Evento click

---
+++ PreBootcamp-Fundamentos-Web/javascript/introduccion_js/script.js
@@ -0,0 +1,41 @@
+// let edad = 50;
+// let apellido = "Gonzalez";
+// let meGustaJs = true;
+
+// console.log("Su edad es:", edad);
+// console.log("Su apellido es:", apellido);
+// console.log("¿Le gusta JavaScript?", meGustaJs);
+
+// let suma = 10 + 4;
+// console.log("La suma es:", suma);
+
+// let edad = 26;
+// let nombre = "Sebastián";
+
+// console.log("Mi nombre es", nombre, "y tengo", edad, "años.");
+
+// let multiplicacion = 25 * 4;
+// let division = 20 / 12;
+
+// console.log("El resultado de la multiplicación es:", multiplicacion);
+// console.log("El resultado de la división es:", division);
+
+// "PAPOMUDAS"
+// "PA = Paréntesis"
+// "PO = Potencia"
+// "MU = Multiplicación"
+// "D = División"
+// "A = Adición"
+// "S = Sustracción"
+
+// let resultado = (5 + 2) * 6;
+// console.log("El resultado de la operación es:", resultado);
+
+// let resultado = 5 * 3 + 10 / 2 - 1;
+// "15 + 5 - 1"
+// console.log("El resultado de la operación es:", resultado);
+
+// let total = 10;
+
+// total = total + 4;
+// total += 4;
