a7d9c1e8039aeb6f3ec7f9577ae88b24066637b9
Sr Jengibre <spobleteobando@gmail.com>
2025-10-06 20:57:45 -0300

    session

---
+++ Python-Fullstack/flask/post/server.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect, session
+
+app = Flask(__name__)
+app.secret_key = "Esto es una clave secreta!"
+
+# La ruta raíz renderizará nuestro formulario
+@app.route('/')
+def index():
+   return render_template("index.html")
+
+# /crear_usuario recibe la información
+
+@app.route('/crear_usuario', methods=['POST'])
+def crear_usuario():
+   print("Recibiendo información")
+   print(request.form)
+   session['nombre_usuario'] = request.form['nombre']
+   session['email_usuario'] = request.form['email']
+   return redirect('/mostrar_usuario') #Redirigimos a otra ruta
+
+@app.route('/mostrar_usuario')
+def mostrar_usuario():
+   print("Usuario redirigido")
+   print(session['nombre_usuario'])
+   print(session['email_usuario'])
+   return render_template("mostrar.html")
+
+if __name__ == "__main__":
+
+   app.run(debug=True)
